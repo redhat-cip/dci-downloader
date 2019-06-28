109a2fd74c0a87d5fbc9b69816a65c63140f8fdb
Guillaume Vincent <gvincent@redhat.com>
2019-06-28 14:48:25 +0200

    First commit
    Change-Id: I80869b1923d5edfcf29d182aa065dd3969075e60

---
+++ dcirc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+DCI_CLIENT_ID='remoteci/d391784b-2cec-44cb-93f8-ce0f25a36546'
+DCI_API_SECRET='LSL0ov00qLUCLZnxhdhCrTwsi1I96SxhdjhekdaGmHdr15B5xLFc0Bl5lXnIhNLp'
+DCI_CS_URL='https://api.distributed-ci.io/'
+LOCAL_STORAGE_FOLDER='/var/lib/dci'
+export DCI_CLIENT_ID
+export DCI_API_SECRET
+export DCI_CS_URL
+export LOCAL_STORAGE_FOLDER
