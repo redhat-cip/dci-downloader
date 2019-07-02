818e39966521480bd23c88fdef6fb974a8523fd3
Guillaume Vincent <gvincent@redhat.com>
2019-07-02 11:03:36 +0200

    First commit
    Change-Id: I80869b1923d5edfcf29d182aa065dd3969075e60

---
+++ dci_downloader/cli.py
@@ -0,0 +1,6 @@
+def parse_arguments(arguments):
+    topics = {}
+    for argument, topic_name in zip(arguments, arguments[1:]):
+        if argument == "--topic":
+            topics[topic_name] = {"name": topic_name}
+    return {"topics": topics}
