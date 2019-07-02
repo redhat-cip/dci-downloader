86cb505f40b1c83f8b8f1f71bca70be5aa62539e
Guillaume Vincent <gvincent@redhat.com>
2019-07-02 11:03:36 +0200

    First commit
    Change-Id: I80869b1923d5edfcf29d182aa065dd3969075e60

---
+++ README.md
@@ -0,0 +1,105 @@
+# DCI Downloader
+
+`dci-downloader` is a useful tool for downloading the latest versions of Red Hat products
+
+## TLDR
+
+```console
+$ sudo yum -y install https://packages.distributed-ci.io/dci-release.el7.noarch.rpm
+$ sudo yum -y install dci-downloader
+$ sudo mv /etc/dci-downloader/dcirc.sh.dist /etc/dci-downloader/dcirc.sh
+$ sudo vi /etc/dci-downloader/dcirc.sh
+$ sudo LOCAL_STORAGE_FOLDER="/var/www/html" dci-downloader --topic "RHEL-7.7"
+```
+
+## Table of Contents
+
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Usage](#usage)
+- [License](#license)
+- [Contact](#contact)
+
+## Requirements
+
+### Red Hat SSO
+
+DCI is connected to the Red Hat SSO. You will need a [Red Hat account](https://access.redhat.com/).
+
+### Remoteci
+
+A remoteci is a Virtual Machine or a baremetal server running RHEL.
+You should check that your remoteci:
+
+- Is running the latest RHEL 7 release.
+- Has a static IPv4 address.
+- Has 160GB of free space in `/var`.
+- Should be able to reach:
+  - `https://api.distributed-ci.io` (443).
+  - `https://packages.distributed-ci.io` (443).
+  - `https://registry.distributed-ci.io` (443).
+  - `https://quay.io` (443).
+  - RedHat CDN.
+
+## Installation
+
+The `dci-downloader` is packaged and available as a RPM files.
+
+```console
+$ yum -y install https://packages.distributed-ci.io/dci-release.el7.noarch.rpm
+$ yum -y install dci-downloader
+```
+
+## Configuration
+
+### Remoteci creation
+
+DCI is connected to the Red Hat SSO. You need to log in `https://www.distributed-ci.io` with your redhat.com SSO account.
+Your user account will be created in our database the first time you connect.
+
+After the first connection you can create a remoteci. Go to [https://www.distributed-ci.io/remotecis](https://www.distributed-ci.io/remotecis) and click `Create a new remoteci` button. Once your `remoteci` is created, you can retrieve the connection information in the `Authentication` column. Edit the `/etc/dci-downloader/dcirc.sh.dist` file with the information displayed and rename it to `/etc/dci-downloader/dcirc.sh`.
+
+At this point, you can validate your credentials with the following commands:
+
+```console
+$ source /etc/dci-downloader/dcirc.sh
+$ dcictl remoteci-list
+```
+
+If you see your remoteci in the list, everything is working great so far.
+
+### Topic access
+
+Before using dci-downloader, we need to check the list of topic (version of the product) you have access to:
+
+```console
+$ source /etc/dci-downloader/dcirc.sh
+$ dcictl topic-list
+```
+
+If you don't see any topic then **you should contact your EPM at Red Hat** which will give you access to the topic you need.
+
+## Usage
+
+You can now download the latest version of a product using dci-downloader
+
+```console
+$ su - dci -s /bin/bash
+$ sudo dci-downloader --topic "RHEL-7.7"
+```
+
+Product will be downloaded in `/var/lib/dci`. You can customize this changing the `LOCAL_STORAGE_FOLDER` env variable
+
+```console
+$ LOCAL_STORAGE_FOLDER="/var/www/html" sudo dci-downloader --topic "RHEL-7.7"
+```
+
+## License
+
+Apache License, Version 2.0 (see [LICENSE](LICENSE) file)
+
+## Contact
+
+Email: Distributed-CI Team <distributed-ci@redhat.com>
+IRC: #distributed-ci on Freenode
---
+++ dci_downloader/downloader.py
@@ -0,0 +1,107 @@
+import errno
+import hashlib
+import os
+import shutil
+
+import requests
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def _sha256_hexdigest_file(filepath):
+    m = hashlib.sha256()
+    with open(filepath, mode="rb") as fd:
+        for data in fd:
+            m.update(data)
+    return m.hexdigest()
+
+
+def _file_exists(file_path):
+    return os.path.exists(file_path)
+
+
+def _file_clean(file_path, sha256):
+    return _sha256_hexdigest_file(file_path) == sha256
+
+
+def get_files_to_download(path, files_list):
+    files_to_download = []
+    for file in files_list["files"]:
+        file_path = os.path.join(path, file["path"], file["name"])
+        if os.path.exists(file_path):
+            continue
+        files_to_download.append(file)
+    return files_to_download
+
+
+def get_files_to_remove(path, files_list):
+    files_list_paths = {}
+    for file in files_list["files"]:
+        file_path = os.path.join(path, file["path"], file["name"])
+        files_list_paths[file_path] = file
+    files_to_remove = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if os.path.islink(file_path):
+                continue
+            if file_path not in files_list_paths or not _file_clean(
+                file_path, files_list_paths[file_path]["sha256"]
+            ):
+                files_to_remove.append(file_path)
+    return files_to_remove
+
+
+def delete_all_symlink_in_path(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if os.path.islink(file_path):
+                os.unlink(file_path)
+        for dir in dirs:
+            folder_path = os.path.join(root, dir)
+            if os.path.islink(folder_path):
+                os.unlink(folder_path)
+
+
+def download_file(url, file_path, cert, key):
+    with requests.get(url, stream=True, cert=(cert, key)) as r:
+        with open(file_path, "wb") as f:
+            shutil.copyfileobj(r.raw, f)
+
+
+def recreate_symlinks(files_list, local_repo_path):
+    for symlink in files_list["symlinks"]:
+        link_path = os.path.join(local_repo_path, symlink["path"], symlink["name"])
+        os.symlink(symlink["destination"], link_path)
+
+
+def download(base_url, files_list, local_repo_path, cert, key):
+    for file in get_files_to_remove(local_repo_path, files_list):
+        print("Remove file %s" % file)
+        os.remove(file)
+
+    delete_all_symlink_in_path(local_repo_path)
+
+    files_to_download = get_files_to_download(local_repo_path, files_list)
+    nb_files = len(files_to_download)
+    for index, file in enumerate(files_to_download):
+        file_relative_path = (
+            "%s/%s" % (file["path"], file["name"]) if file["path"] else file["name"]
+        )
+        print("(%d/%d): %s" % (index, nb_files, file_relative_path))
+        local_dir = os.path.join(local_repo_path, file["path"])
+        mkdir_p(local_dir)
+        file_url = "%s/%s" % (base_url, file_relative_path)
+        local_file_path = os.path.join(local_dir, file["name"])
+        download_file(file_url, local_file_path, cert, key)
+
+    recreate_symlinks(files_list, local_repo_path)
