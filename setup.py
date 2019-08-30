19463e626cc184fb40f013b632a65019d5384c44
Haikel Guemar <hguemar@fedoraproject.org>
2019-08-30 10:59:41 +0200

    Add setup.py
    pip refuses to install it without a proper setup.py
    
    Change-Id: I2cd6473bec577c61c410fae3f8694733fb82a0e9

---
+++ setup.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import codecs
+import os
+import setuptools
+
+from dci_downloader import version
+
+
+def _get_requirements():
+    requirements_path = '%s/%s' % (os.path.dirname(os.path.abspath(__file__)),
+                                   'requirements.txt')
+    with open(requirements_path, 'r') as f:
+        requirements = f.read()
+        return requirements.split('\n')
+
+
+def _get_readme():
+    readme_path = '%s/%s' % (os.path.dirname(os.path.abspath(__file__)),
+                             'README.md')
+
+    with codecs.open(readme_path, 'r', encoding='utf8') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='dci-downloader',
+    version=version.__version__,
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    author='Distributed ci team.',
+    author_email='distributed-ci@redhat.com',
+    description='Utility to download Red Hat components from DCI',
+    long_description=_get_readme(),
+    install_requires=_get_requirements(),
+    url='https://github.com/redhat-cip/dci-downloader',
+    license='Apache v2.0',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Distributed Computing'
+    ],
+    scripts=[
+        'bin/dci-downloader'
+    ])
