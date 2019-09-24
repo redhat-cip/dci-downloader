674e8038e93cc280a3d9a960d876d7213accf5a8
Guillaume Vincent <gvincent@redhat.com>
2019-09-24 17:13:11 +0200

    Make retro tests more robuts
    Depends-On: I42c5c057c8838f2d4148f7c294bb4dfe230ac516
    
    Change-Id: I15ee3d1822c5d2c68af929cb8987ffb8d1f4485e

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -21,10 +21,9 @@ def test_reading_settings_file():
     assert settings == {
         "archs": ["x86_64", "ppc64le"],
         "dci_rhel_agent_cert": False,
-        "download_only": False,
-        "local_repo_ip": "172.23.100.100",
-        "systems": ["labvm.local"],
-        "topic": "RHEL-7",
+        "local_repo_ip": "192.168.1.1",
+        "systems": ["dci-client"],
+        "topic": "RHEL-7.6",
         "variants": ["AppStream", "BaseOS"],
         "with_debug": False,
     }
@@ -95,3 +94,32 @@ def test_get_settings_from_env_variables_retro_compatibility():
         },
     )
     assert settings["destination_folder"] == "/tmp/repo"
+    assert settings["archs"] == ["x86_64"]
+    assert settings["variants"] == []
+    assert settings["remoteci_id"] == "9dd94b70-1707-46c5-a2bb-661e8d5d4212"
+    assert not settings["download_everything"]
+    assert not settings["with_debug"]
+    assert settings["topic_name"] == "RHEL-7"
+    assert settings["topic"] == "RHEL-7"
+
+
+def test_get_settings_from_env_variables_retro_compatibility_rhel_agent_settings():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    settings_file_path = os.path.join(test_dir, "data", "settings.yml")
+    settings = get_settings(
+        sys_args=["--settings", settings_file_path],
+        env_variables={
+            "DCI_CLIENT_ID": "remoteci/9dd94b70-46c5-1707-a2bb-661e8d5d4212",
+            "DCI_API_SECRET": "",
+            "DCI_CS_URL": "",
+            "DCI_LOCAL_REPO": "/tmp/repo2",
+        },
+    )
+    assert settings["destination_folder"] == "/tmp/repo2"
+    assert settings["archs"] == ["x86_64", "ppc64le"]
+    assert settings["variants"] == ["AppStream", "BaseOS"]
+    assert settings["remoteci_id"] == "9dd94b70-46c5-1707-a2bb-661e8d5d4212"
+    assert not settings["download_everything"]
+    assert not settings["with_debug"]
+    assert settings["topic_name"] == "RHEL-7.6"
+    assert settings["topic"] == "RHEL-7.6"
