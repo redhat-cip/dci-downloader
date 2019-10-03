a85517118c7ab2b0b30a369038f4b967e9d83f5e
Guillaume Vincent <gvincent@redhat.com>
2019-10-03 08:44:50 +0200

    Feature: Multiple topics in settings file
    This patch allows a user to specify multiple topics in his settings file.
    Keep backward compatibility with previous settings files.
    Rename destination_folder into download_folder
    
    Change-Id: I42339b37bbf3d39ddb96d79f063f99ad49ec88e7

--- dci_downloader/downloader.py
+++ dci_downloader/downloader.py
@@ -3,43 +3,43 @@
 import os
 
 from api import get_files_list, get_base_url, download_file
-from stats import check_destination_folder_size
+from stats import check_download_folder_size
 from filters import filter_files_list
 from files_list import get_files_to_download, get_files_to_remove
 from fs import (
     mkdir_p,
     delete_all_symlink_in_path,
     recreate_symlinks,
-    build_destination_folder,
+    build_download_folder,
     create_parent_dir,
 )
 
 
-def clean_destination_folder(files_list, destination_folder):
-    if not os.path.isdir(destination_folder):
-        mkdir_p(destination_folder)
+def clean_download_folder(files_list, download_folder):
+    if not os.path.isdir(download_folder):
+        mkdir_p(download_folder)
 
-    for file in get_files_to_remove(files_list, destination_folder):
+    for file in get_files_to_remove(files_list, download_folder):
         print("Remove file %s" % file)
         os.remove(file)
 
-    delete_all_symlink_in_path(destination_folder)
+    delete_all_symlink_in_path(download_folder)
 
 
 def download_component(topic, component, settings, cert, key):
     print("Download component %s" % component["name"])
     base_url = get_base_url(topic, component)
     files_list = get_files_list(base_url, cert, key)
-    destination_folder = build_destination_folder(
-        topic, component, settings["destination_folder"]
+    download_folder = build_download_folder(
+        topic, component, settings["download_folder"]
     )
-    clean_destination_folder(files_list, destination_folder)
+    clean_download_folder(files_list, download_folder)
     files_list = filter_files_list(files_list, settings)
-    check_destination_folder_size(files_list, destination_folder)
-    files_to_download = get_files_to_download(base_url, destination_folder, files_list)
+    check_download_folder_size(files_list, download_folder)
+    files_to_download = get_files_to_download(base_url, download_folder, files_list)
     nb_files = len(files_to_download)
     for index, file in enumerate(files_to_download):
         print("(%d/%d): %s" % (index, nb_files, file["destination"]))
         create_parent_dir(file["destination"])
         download_file(file, cert, key)
-    recreate_symlinks(files_list["symlinks"], destination_folder)
+    recreate_symlinks(files_list["symlinks"], download_folder)
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,9 +1,9 @@
-from dci_downloader.fs import build_destination_folder
+from dci_downloader.fs import build_download_folder
 
 
 def test_get_base_url():
     topic = {"id": "t1", "name": "topic 1"}
     component = {"id": "c1", "type": "Compose"}
-    destination_folder = build_destination_folder(topic, component, "/var/www/html")
-    expected_destination_folder = "/var/www/html/topic_1/compose"
-    assert destination_folder == expected_destination_folder
+    download_folder = build_download_folder(topic, component, "/var/www/html")
+    expected_download_folder = "/var/www/html/topic_1/compose"
+    assert download_folder == expected_download_folder
