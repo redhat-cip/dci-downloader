fce9f1ebef5fdc48fde0278d4469656b83c58275
Guillaume Vincent <gvincent@redhat.com>
2020-06-01 14:38:57 +0200

    Stop refreshing SSL certificates
    If crt and key certificates are found at DCI_KEY_FILE and DCI_CERT_FILE destination, dci-downloader skip the SSL refresh.
    Python requests use this file to initiate the SSL communication with repo.distributed-ci.io.
    If env variable are not setted $XDG_DATA_HOME/dci-downloader/dci.(key/crt) or ~/.local/share/dci-downloader/dci.(key/crt) are used by default.
    
    Change-Id: I5b09bfa5381fb3f9899e9d13d337dd8d38f652cf

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,7 +1,4 @@
-import os
-import pytest
-
-from dci_downloader.fs import build_download_folder, create_temp_file
+from dci_downloader.fs import build_download_folder
 
 
 def test_build_download_folder():
@@ -10,15 +7,3 @@ def test_build_download_folder():
     download_folder = build_download_folder(topic, component, "/var/www/html")
     expected_download_folder = "/var/www/html/topic_1/compose"
     assert download_folder == expected_download_folder
-
-
-def test_create_temp_file_with_non_bytes_input():
-    try:
-        content = u"test"
-        cert = create_temp_file(content)
-        os.remove(cert.name)
-    except TypeError:
-        pytest.fail(
-            "test_create_temp_file_with_non_bytes_input "
-            "raise TypeError but should not"
-        )
