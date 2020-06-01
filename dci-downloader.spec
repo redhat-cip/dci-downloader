c9b066d6c64db3e266b4457b63c95839c5c52c5e
Guillaume Vincent <gvincent@redhat.com>
2020-06-01 14:38:57 +0200

    Stop refreshing SSL certificates
    If crt and key certificates are found at DCI_KEY_FILE and DCI_CERT_FILE destination, dci-downloader skip the SSL refresh.
    Python requests use this file to initiate the SSL communication with repo.distributed-ci.io.
    If env variable are not setted $XDG_DATA_HOME/dci-downloader/dci.(key/crt) or ~/.local/share/dci-downloader/dci.(key/crt) are used by default.
    
    Change-Id: I5b09bfa5381fb3f9899e9d13d337dd8d38f652cf

--- dci-downloader.spec
+++ dci-downloader.spec
@@ -6,7 +6,7 @@
 %global srcname dci-downloader
 
 Name:             dci-downloader
-Version:          2.3.1
+Version:          2.4.0
 Release:          2.VERS%{?dist}
 Summary:          DCI Downloader
 License:          ASL 2.0
@@ -68,6 +68,8 @@ DCI downloader used to download Red Hat products
 %{_bindir}/%{srcname}
 
 %changelog
+* Tue Aug 25 2020 Guillaume Vincent <gvincent@redhat.com> - 2.4.0-1
+- Stop refreshing SSL certificates
 * Thu Jul 30 2020 Guillaume Vincent <gvincent@redhat.com> - 2.3.1-1
 - Fix bad exception raised
 * Mon Jun 22 2020 Guillaume Vincent <gvincent@redhat.com> - 2.3.0-1
--- dci_downloader/main.py
+++ dci_downloader/main.py
@@ -7,8 +7,8 @@ import traceback
 
 from dci_downloader import api
 from dci_downloader import downloader
+from dci_downloader.certificates import configure_ssl_certificates
 from dci_downloader.settings import get_settings, exit_if_settings_invalid
-from dci_downloader.fs import create_temp_file
 
 
 def signal_handler(sig, frame):
@@ -20,7 +20,7 @@ signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
 
-def download_components(settings, api, downloader, cert, key):
+def download_components(settings, api, downloader):
     if "component_id" in settings and settings["component_id"]:
         component_id = settings["component_id"]
         component = api.get_component_by_id(component_id)
@@ -31,18 +31,13 @@ def download_components(settings, api, downloader, cert, key):
         topic = api.get_topic(settings["name"])
         components = api.get_components(topic)
     for component in components:
-        downloader.download_component(topic, component, settings, cert, key)
+        downloader.download_component(topic, component, settings)
 
 
 def main():
     settings = get_settings(sys_args=sys.argv[1:], env_variables=dict(os.environ))
     exit_if_settings_invalid(settings)
-    keys = api.get_keys(settings["remoteci_id"])
-    if keys is None:
-        print("Can't get certificate's keys, contact DCI administrator")
-        sys.exit(0)
-    cert = create_temp_file(keys["cert"]).name
-    key = create_temp_file(keys["key"]).name
+    configure_ssl_certificates(settings)
     return_code = 0
     for topic_settings in settings["topics"]:
         topic_name = topic_settings["name"]
@@ -50,13 +45,11 @@ def main():
             topic = api.get_topic(topic_name)
             if topic is None:
                 raise Exception("Topic name %s not found" % topic_name)
-            download_components(topic_settings, api, downloader, cert, key)
+            download_components(topic_settings, api, downloader)
         except Exception:
             print("Exception when downloading components for %s" % topic_name)
             traceback.print_exc()
             return_code = 1
-    os.unlink(cert)
-    os.unlink(key)
     sys.exit(return_code)
 
 
