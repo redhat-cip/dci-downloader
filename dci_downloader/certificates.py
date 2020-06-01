3865a998ed221371f3a404bf0ee2287435c306a4
Guillaume Vincent <gvincent@redhat.com>
2020-06-01 14:38:57 +0200

    WIP Stop refreshing SSL certificates
    Change-Id: I5b09bfa5381fb3f9899e9d13d337dd8d38f652cf

---
+++ dci_downloader/certificates.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import api
+
+
+def configure_ssl_certificates(settings, env_variables):
+    dci_cert_file = settings.get("dci_cert_file")
+    env_variables["REQUESTS_CA_BUNDLE"] = dci_cert_file
+    if not os.path.exists(dci_cert_file):
+        keys = api.get_keys(settings["remoteci_id"])
+        if keys is None:
+            print("Can't get certificate's keys, contact DCI administrator")
+            sys.exit(1)
+
+        cert = keys["cert"]
+        key = keys["key"]
+        with open(dci_cert_file, "w") as f:
+            f.write(cert)
+            f.write("\n")
+            f.write(key)
