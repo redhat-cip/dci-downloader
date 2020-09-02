cd6d0f5c99c7238369f2f491081c520db30e9395
Guillaume Vincent <gvincent@redhat.com>
2020-09-02 17:09:24 +0200

    Add file lock to avoid to download 2 topics simultaneously
    Use file lock per topic. If file lock is present we wait 30s and check again.
    We do this during 10 hours before we stop waiting.
    
    This one allow complete parallel download
    
    Change-Id: I5c8d450698064a26053f5f828e975949687acdf0

--- dci_downloader/fs.py
+++ dci_downloader/fs.py
@@ -1,5 +1,6 @@
 import errno
 import os
+import fcntl
 
 
 def mkdir_p(path):
@@ -36,7 +37,27 @@ def recreate_symlinks(symlinks, destination_path):
         os.symlink(symlink["destination"], link_path)
 
 
-def build_download_folder(topic, component, download_folder):
-    topic_name = topic["name"].strip().replace(" ", "_")
+def get_topic_folder(settings):
+    topic_name = settings["name"].strip().replace(" ", "_")
+    download_folder = settings["download_folder"]
+    return os.path.join(download_folder, topic_name)
+
+
+def get_component_folder(settings, component):
+    topic_folder = get_topic_folder(settings)
     component_type = component["type"].strip().replace(" ", "_").lower()
-    return os.path.join(download_folder, topic_name, component_type)
+    return os.path.join(topic_folder, component_type)
+
+
+class file_lock(object):
+    def __init__(self, file_path):
+        self.lock_fd = open(file_path, "a")
+        fcntl.flock(self.lock_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+    def __enter__(self):
+        return self.lock_fd
+
+    def __exit__(self, type, value, traceback):
+        os.remove(self.lock_fd.name)
+        fcntl.flock(self.lock_fd, fcntl.LOCK_UN)
+        self.lock_fd.close()
