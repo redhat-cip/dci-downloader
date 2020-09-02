70f504fd18ca216914d0f101beb7ba7cb7465dec
Guillaume Vincent <gvincent@redhat.com>
2020-09-02 17:09:24 +0200

    Add file lock to avoid to download the same topic in two processes simultaneously
    When we try to download the same topic in two differents processes,
    we can encounter issues and race conditions. Now we use one file lock per topic.
    If file lock is present we wait 30s and check again.
    We do this during 10 hours before we stop waiting.
    
    This one allow complete parallel downloads.
    
    Change-Id: I5c8d450698064a26053f5f828e975949687acdf0

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,21 +3,21 @@ import mock
 from dci_downloader.main import download_components
 
 
-def test_download_components_if_no_component_id():
-    api = mock.MagicMock()
+@mock.patch("dci_downloader.main.api")
+@mock.patch("dci_downloader.main.downloader")
+def test_download_components_if_no_component_id(downloader_mock, api_mock):
     topic = {"id": "t1", "name": "RHEL-8"}
-    api.get_topic.return_value = topic
+    api_mock.get_topic.return_value = topic
     component1 = {"id": "c1", "name": "RHEL-8.2.0-20191120.0", "topic_id": "t1"}
     component2 = {"id": "c2", "name": "hwcert-1575558974", "topic_id": "t1"}
-    api.get_components.return_value = [component1, component2]
-    downloader = mock.MagicMock()
+    api_mock.get_components.return_value = [component1, component2]
     settings = {
         "name": "RHEL-8",
         "download_folder": "/var/www/html",
     }
-    download_components(settings, api, downloader)
-    api.get_topic.assert_called_once_with("RHEL-8")
-    downloader.download_component.assert_has_calls(
+    download_components(settings)
+    api_mock.get_topic.assert_called_once_with("RHEL-8")
+    downloader_mock.download_component.assert_has_calls(
         [
             mock.call(topic, component1, settings),
             mock.call(topic, component2, settings),
@@ -25,18 +25,20 @@ def test_download_components_if_no_component_id():
     )
 
 
-def test_download_one_component_if_component_id():
-    api = mock.MagicMock()
+@mock.patch("dci_downloader.main.api")
+@mock.patch("dci_downloader.main.downloader")
+def test_download_one_component_if_component_id(downloader_mock, api_mock):
     component = {"id": "c1", "name": "RHEL-8.2.0-20191120.0", "topic_id": "t1"}
-    api.get_component_by_id.return_value = component
+    api_mock.get_component_by_id.return_value = component
     topic = {"id": "t1", "name": "RHEL-8"}
-    api.get_topic_by_id.return_value = topic
-    downloader = mock.MagicMock()
+    api_mock.get_topic_by_id.return_value = topic
     settings = {
         "component_id": "c1",
         "download_folder": "/var/www/html",
     }
-    download_components(settings, api, downloader)
-    api.get_component_by_id.assert_called_once_with(component["id"])
-    api.get_topic_by_id.assert_called_once_with(topic["id"])
-    downloader.download_component.assert_called_once_with(topic, component, settings)
+    download_components(settings)
+    api_mock.get_component_by_id.assert_called_once_with(component["id"])
+    api_mock.get_topic_by_id.assert_called_once_with(topic["id"])
+    downloader_mock.download_component.assert_called_once_with(
+        topic, component, settings
+    )
