cfd5243774c5450ecde9c70dcb5f4d2251733823
Guillaume Vincent <gvincent@redhat.com>
2021-01-28 16:55:48 +0100

    Allow debug flag without variant
    A user can now download all debug files for a specific arch
    
    Change-Id: I58aba04a2ddbfd44f46357852b8f7a237dcf936f

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -128,6 +128,61 @@ def test_filter_files_list_with_debug():
     assert filter_files_list(dci_files_list, settings) == expected_files_list
 
 
+def test_filter_files_list_with_debug_keep_os_folder_nrt():
+    dci_files_list = {
+        "directories": [],
+        "files": [
+            {
+                "path": "AppStream/x86_64/debug/tree/Packages",
+                "sha256": "6f48f0d285918e502035da74decf447c6bb29898206406a4ed6a92ece94d276a",
+                "name": "PackageKit-command-not-found-debuginfo-1.1.12-2.el8.x86_64.rpm",
+                "size": 45052,
+            },
+            {
+                "path": "AppStream/x86_64/os/Packages",
+                "sha256": "8fe293470f677bfc6eb04204c47b5e1a0e5d15431ef7ed9dbb269aaea386ed9f",
+                "name": "PackageKit-command-not-found-1.1.12-2.el8.x86_64.rpm",
+                "size": 28616,
+            },
+            {
+                "path": "CRB/ppc64le/os/repodata",
+                "sha256": "b033f8c928c3a5ba43c0a0e87a839b5ce24698e8c0a7c9c4a33e564040231805",
+                "name": "repomd.xml",
+                "size": 3268,
+            },
+            {
+                "path": "CRB/ppc64le/debug/tree/repodata",
+                "sha256": "974ebd02506a4945d4f1a7ac8ac5d5a05e675e0e4bd860de667425adf02d2570",
+                "name": "repomd.xml",
+                "size": 1562,
+            },
+        ],
+        "symlinks": [],
+    }
+    args = ["RHEL-8", "/tmp", "--arch=ppc64le", "--debug"]
+    settings = get_settings(sys_args=args)["topics"][0]
+    expected_files_list = {
+        "directories": [],
+        "files": [
+            {
+                "path": "CRB/ppc64le/os/repodata",
+                "sha256": "b033f8c928c3a5ba43c0a0e87a839b5ce24698e8c0a7c9c4a33e564040231805",
+                "name": "repomd.xml",
+                "size": 3268,
+            },
+            {
+                "path": "CRB/ppc64le/debug/tree/repodata",
+                "sha256": "974ebd02506a4945d4f1a7ac8ac5d5a05e675e0e4bd860de667425adf02d2570",
+                "name": "repomd.xml",
+                "size": 1562,
+            },
+        ],
+        "symlinks": [],
+    }
+
+    assert filter_files_list(dci_files_list, settings) == expected_files_list
+
+
 def test_filter_files_list_with_iso():
     dci_files_list = {
         "directories": [],
@@ -334,9 +389,9 @@ def test_nrt_always_download_metadata():
         ],
         "symlinks": [],
     }
-    settings = get_settings(
-        sys_args=["RHEL-8", "/tmp", "--variant", "AppStream"]
-    )["topics"][0]
+    settings = get_settings(sys_args=["RHEL-8", "/tmp", "--variant", "AppStream"])[
+        "topics"
+    ][0]
     expected_files_list = {
         "directories": [],
         "files": [
