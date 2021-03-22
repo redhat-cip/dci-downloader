f270264668e2848245ebe04253b024537481c1b6
François Charlier <francois.charlier@redhat.com>
2021-03-22 17:56:34 +0100

    Mirror container images for supported topics
    If "--registry <...>" is passed on the command-line or in config file or
    in DCI_REGISTRY environment variable, dci-downloader will mirror the
    container images for the components having some.
    
    This requires a working anonymous registry service running at the specified URL.
    
    This implementation will only work if skopeo with the sync subcommand is
    available (>= 0.1.41) and will display a warning if skopeo is not found
    or an incompatible skopeo is installed.
    
    Change-Id: I97c1e764dc46d2bd061fe490abb21729680f25cf

---
+++ tests/test_containers.py
@@ -0,0 +1,42 @@
+from dci_downloader.containers import (
+    dci_images_list_yaml_to_skopeo_format,
+    dci_image_info,
+    has_skopeo_command,
+)
+
+
+def test_dci_image_info():
+    dci_image = "registry.distributed-ci.io/rhosp16/openstack-base:16.2_20210215.1"
+    image_info = dci_image_info(dci_image)
+    assert image_info == {
+        "registry": "registry.distributed-ci.io",
+        "image": "rhosp16/openstack-base",
+        "tag": "16.2_20210215.1",
+    }
+
+
+def test_images_list_yaml_to_skopeo_yaml():
+    images_list_yaml = """---
+- registry.distributed-ci.io/rhosp16/openstack-base:16.2_20210215.1
+- registry.distributed-ci.io/rhosp16/openstack-barbican-base:16.2_20210215.1
+- registry.distributed-ci.io/rhosp16/openstack-barbican-api:16.2_20210215.1
+- registry.distributed-ci.io/rhosp16/openstack-barbican-keystone-listener:16.2_20210215.1
+- registry.distributed-ci.io/rhosp16/openstack-barbican-worker:16.2_20210215.1
+"""
+    skopeo_format = dci_images_list_yaml_to_skopeo_format(
+        images_list_yaml, "toto", "superhardtoguess"
+    )
+    assert "registry.distributed-ci.io" in skopeo_format.keys()
+    reg = skopeo_format["registry.distributed-ci.io"]
+    assert "credentials" in reg
+    assert "username" in reg["credentials"]
+    assert reg["credentials"]["username"] == "toto"
+    assert "password" in reg["credentials"]
+    assert reg["credentials"]["password"] == "superhardtoguess"
+    assert "images" in reg
+    assert len(reg["images"]) == 5
+
+
+def test_has_skopeo_command():
+    assert has_skopeo_command("meh") is False
+    assert has_skopeo_command("", skopeo_bin="/bin/ls") is True
